c1710e828f4e31f74e1b06eb77215807c7257cdb
Pedro Machado da Silva <pedro_masi@yahoo.com.br>
2023-06-29 13:54:35 -0300

    CSS finalizado, iniciando o JS

---
+++ script.js
@@ -0,0 +1,17 @@
+//256de7a7298e1900f0883c7aa72b160d
+
+const container = document.querySelector('container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+    
+    const APIKey = '256de7a7298e1900f0883c7aa72b160d';
+    const city = document.querySelector('.search-box input').value;
+
+
+
+
+})
