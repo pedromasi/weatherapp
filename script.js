86ca46141419eeb352cdc95c609612c5fa144f69
Pedro Machado da Silva <pedro_masi@yahoo.com.br>
2023-06-30 09:42:11 -0300

    Parei no switch para as imagens do tempo

--- script.js
+++ script.js
@@ -10,8 +10,33 @@ search.addEventListener('click', () => {
     
     const APIKey = '256de7a7298e1900f0883c7aa72b160d';
     const city = document.querySelector('.search-box input').value;
+    
 
+    fetch(`https://openweathermap.org/data/2.5/weather?q=${city}&units=metric&ppid=${APIKey}`).then(response => response.json()).then(json => {
+        if(json.cod === '404'){
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('FadeIn');
+            return;
+        }
+        error404.style.display = 'none';
+        error404.classList.remove('FadeIn');
 
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-box .humidity span');
+        const wind = document.querySelector('.weather-box .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/clear.png';
+                break;
+        }
+
+    })
 
 
 })
